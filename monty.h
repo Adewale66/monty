b52b36066e591db72ccf240173297f0ba9ee6e5d
Adewale Kujore <adewalekujore6@gmail.com>
2023-12-13 13:20:00 +0100

    1 - 14

--- monty.h
+++ monty.h
@@ -62,6 +62,10 @@ void sub(stack_t **head, unsigned int line_number);
 void divide(stack_t **head, unsigned int line_number);
 void mul(stack_t **head, unsigned int line_number);
 void mod(stack_t **head, unsigned int line_number);
+void pchar(stack_t **head, unsigned int line_number);
+void pstr(stack_t **head, unsigned int line_number);
+void rot1(stack_t **head, unsigned int line_number);
+void rotr(stack_t **head, unsigned int line_number);
 
 /* Function Prototypes */
 void parse(FILE *file);
--- operation3.c
+++ operation3.c
@@ -32,3 +32,98 @@ void mod(stack_t **head, unsigned int line_number)
 	free(current);
 	new_node_n = 0;
 }
+
+/**
+ * pchar - prints the char at the top of the stack, followed by a new line.
+ * @head: pointer to the head of the stack
+ * @line_number: line number of the opcode
+ * Return: void
+ */
+
+void pchar(stack_t **head, unsigned int line_number)
+{
+	stack_t *current = *head;
+
+	if (current == NULL)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+		free_stack(head);
+		new_node_n = INVALID_OPCODE;
+		return;
+	}
+	if (current->n < 0 || current->n > 127)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+		free_stack(head);
+		new_node_n = INVALID_OPCODE;
+		return;
+	}
+	printf("%c\n", current->n);
+	new_node_n = 0;
+}
+
+/**
+ * pstr - prints the string starting at the top of the stack,
+ * followed by a new line.
+ * @head: pointer to the head of the stack
+ * @line_number: line number of the opcode
+ * Return: void
+ */
+
+void pstr(stack_t **head, unsigned int line_number)
+{
+	stack_t *c = *head;
+
+	(void)line_number;
+	while (c != NULL && c->n != 0 && c->n >= 0 && c->n <= 127)
+	{
+		printf("%c", c->n);
+		c = c->next;
+	}
+	printf("\n");
+	new_node_n = 0;
+}
+
+/**
+ * rot1 - rotates the stack to the top.
+ * @head: pointer to the head of the stack
+ * @line_number: line number of the opcode
+ * Return: void
+ */
+
+void rot1(stack_t **head, unsigned int line_number)
+{
+	stack_t *current = *head;
+	int tmp;
+
+	(void)line_number;
+	if (current == NULL || current->next == NULL)
+		return;
+	tmp = current->n;
+	current->n = current->next->n;
+	current->next->n = tmp;
+	new_node_n = 0;
+}
+
+/**
+ * rotr - rotates the stack to the bottom.
+ * @head: pointer to the head of the stack
+ * @line_number: line number of the opcode
+ * Return: void
+ */
+
+void rotr(stack_t **head, unsigned int line_number)
+{
+	stack_t *current = *head;
+	int tmp;
+
+	(void)line_number;
+	if (current == NULL || current->next == NULL)
+		return;
+	while (current->next != NULL)
+		current = current->next;
+	tmp = current->n;
+	current->n = current->prev->n;
+	current->prev->n = tmp;
+	new_node_n = 0;
+}
